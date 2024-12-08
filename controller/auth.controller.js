acb5ab1a07151702bf4f75b526984fb8ccb87336
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-08 10:48:50 +0600

    remove unusal code from user model & auth controller

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,28 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-// export const signup = async (req, res) => {
-//     const { userName, email, password } = req.body;
-
-//     if (!userName || !email || !password || userName === "" || email === "" || password === "") {
-//         return res.status(400).json({ message: "All fields are required" });
-//     }
-
-//     const hashedPassword = bcryptjs.hashSync(password, 10)
-
-//     const newUser = new User({
-//         userName,
-//         email,
-//         password: hashedPassword
-//     })
-//     try {
-//         await newUser.save();
-//         res.status(201).json({ message: "User created successfully" });
-//     } catch (error) {
-//         res.status(500).json({ message: error.message });
-//     }
-// }
-
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
 
