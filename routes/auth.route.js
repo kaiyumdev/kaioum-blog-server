6f79af3f0ef6a2f1f09282286278dcc968274ae8
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-10 17:10:10 +0600

    create google route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,5 +3,6 @@ import { signin, signup } from '../controller/auth.controller.js';
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/signin", signin)
+router.post("/signin", signin);
+router.post("google", google)
 export default router;
