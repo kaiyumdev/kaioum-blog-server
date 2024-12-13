675ae3e8befffc71e7ef86814791cf7ebc77e5f8
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-13 18:41:20 +0600

    updated google controller to show user profile at client successfully

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -78,21 +78,42 @@ export const google = async (req, res, next) => {
         } else {
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-            const newUser = new User(
-                {
-                    username: name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
-                    email,
-                    password: hashedPassword,
-                    profilePicture: googlePhotoUrl
-                }
-            )
-            await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
-            const { password, ...rest } = newUser._doc;
-            res.status(200).cookie('access_token', token, {
-                httpOnly: true
-            }).json(rest);
+            //     const newUser = new User(
+            //         {
+            //             username: name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
+            //             email,
+            //             password: hashedPassword,
+            //             profilePicture: googlePhotoUrl
+            //         }
+            //     )
+            //     await newUser.save();
+            //     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
+            //     const { password, ...rest } = newUser._doc;
+            //     res.status(200).cookie('access_token', token, {
+            //         httpOnly: true
+            //     }).json(rest);
 
+            // }
+            const newUser = new User({
+                username:
+                    name.toLowerCase().split(' ').join('') +
+                    Math.random().toString(9).slice(-4),
+                email,
+                password: hashedPassword,
+                profilePicture: googlePhotoUrl,
+            });
+            await newUser.save();
+            const token = jwt.sign(
+                { id: newUser._id },
+                process.env.JWT_SECRET
+            );
+            const { password, ...rest } = newUser._doc;
+            res
+                .status(200)
+                .cookie('access_token', token, {
+                    httpOnly: true,
+                })
+                .json(rest);
         }
     } catch (error) {
         next(error);
